5f8e726042e129caeb6a99b9e4924d50be3f125e
Trevor Brooks <trevorism@gmail.com>
2024-06-04 18:50:23 -0400

    check value

--- src/app/cypress/e2e/spec.cy.js
+++ src/app/cypress/e2e/spec.cy.js
@@ -1,6 +1,17 @@
-describe('home page', () => {
+describe('login page', () => {
     it('passes', () => {
-        cy.visit('https://timeline.draw.trevorism.com')
-        cy.title().should('equal', 'Trevorism Timeline')
+        cy.visit('https://ormodule.bigbear-ai.com')
+        cy.title().should('equal', 'or_module_ui')
+    })
+})
+
+describe('home page', () => {
+    it('logs in successfully', () => {
+        cy.visit('https://ormodule.bigbear-ai.com')
+        cy.get('#username').type(Cypress.env('username'))
+        cy.get('#password').type(Cypress.env('password'))
+        cy.get('button').click()
+        cy.get('app-home').should('exist')
+        cy.get('app-home').contains(Cypress.env('username'))
     })
 })

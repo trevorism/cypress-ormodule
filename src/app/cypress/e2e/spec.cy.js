15c94728f97621550d677680b3cdef93af457d06
Trevor Brooks <trevorism@gmail.com>
2024-06-04 18:58:54 -0400

    Try to pass in username password

--- src/app/cypress/e2e/spec.cy.js
+++ src/app/cypress/e2e/spec.cy.js
@@ -8,10 +8,10 @@ describe('login page', () => {
 describe('home page', () => {
     it('logs in successfully', () => {
         cy.visit('https://ormodule.bigbear-ai.com')
-        cy.get('#username').type(Cypress.env('username'))
-        cy.get('#password').type(Cypress.env('password'))
+        cy.get('#username').type(Cypress.env('USERNAME'))
+        cy.get('#password').type(Cypress.env('PASSWORD'))
         cy.get('button').click()
         cy.get('app-home').should('exist')
-        cy.get('app-home').contains(Cypress.env('username'))
+        cy.get('app-home').contains(Cypress.env('USERNAME'))
     })
 })
